1e8da99c0357d0fc8e8da1cca9eb46d280d37b75
yasu2244 <y.ok.mch@gmail.com>
2025-03-07 04:01:34 +0900

    セットアップとFortify導入

---
+++ src/app/Providers/FortifyServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
+use App\Actions\Fortify\CreateNewUser;
+use App\Actions\Fortify\ResetUserPassword;
+use App\Actions\Fortify\UpdateUserPassword;
+use App\Actions\Fortify\UpdateUserProfileInformation;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * サービスを登録
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * サービスのブート処理
+     */
+    public function boot()
+    {
+        Fortify::createUsersUsing(CreateNewUser::class);
+        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        Fortify::loginView(fn () => view('auth.login'));
+        Fortify::registerView(fn () => view('auth.register'));
+    }
+}
